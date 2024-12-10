a9259c358b8256bd53f902a19b754f7460d4affd
Jayson Mourier <jayson.mourier@proton.me>
2024-12-10 15:18:18 +0100

    fixed button padding

--- src/app/components/VideoButton/VideoButton.tsx
+++ src/app/components/VideoButton/VideoButton.tsx
@@ -5,7 +5,7 @@ import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function VideoButton() {
     return (
-        <div className="bg-white py-2 sm:px-4 rounded-xl font-bold cursor-pointer flex items-center space-x-3 text-xs" onClick={() => window.open("https://youtu.be/4OOuJzf5c_w", "_blank")}>
+        <div className="bg-white px-2 py-1 sm:py-2 sm:px-4 rounded-xl font-bold cursor-pointer flex items-center space-x-3 text-xs" onClick={() => window.open("https://youtu.be/4OOuJzf5c_w", "_blank")}>
             <div className="bg-gray-300 p-2 rounded-full flex items-center justify-center aspect-square">
                 <FontAwesomeIcon icon={faPlay} className="text-xs" />
             </div>
