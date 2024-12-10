a9bc8942baf2bac6fcae56112b1228d1ddf7b4e2
Jayson Mourier <jayson.mourier@proton.me>
2024-12-10 16:05:38 +0100

    for the moment, force light mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        //background: "var(--background)",
+        //foreground: "var(--foreground)",
+        background: "#ffffff",
+        foreground: "#000000"
       },
     },
   },
