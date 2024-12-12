1fe2574ed39970759effe743bc344709d4acf6ff
Jayson Mourier <jayson.mourier@proton.me>
2024-12-12 13:25:04 +0100

    dark mode by default

--- src/app/components/ThemeButton/ThemeButton.tsx
+++ src/app/components/ThemeButton/ThemeButton.tsx
@@ -7,19 +7,19 @@ import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import "@/app/components/ThemeButton/ThemeButton.css"
 
 export default function ThemeButton() {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("dark");
 
     useEffect(() => {
-        const storedTheme = localStorage.getItem("theme") || "light";
+        const storedTheme = localStorage.getItem("theme") || "dark";
         setTheme(storedTheme);
-        document.documentElement.classList.toggle("dark", storedTheme === "dark");
+        document.documentElement.classList.toggle("dark", storedTheme === "light");
     }, []);
 
     const toggleTheme = () => {
-        const newTheme = theme === "light" ? "dark" : "light";
+        const newTheme = theme === "dark" ? "light" : "dark";
         setTheme(newTheme);
         localStorage.setItem("theme", newTheme);
-        document.documentElement.classList.toggle("dark", newTheme === "dark");
+        document.documentElement.classList.toggle("light", newTheme === "light");
     };
 
     return (
@@ -27,7 +27,7 @@ export default function ThemeButton() {
             onClick={toggleTheme}
             className="fixed w-[50px] h-[50px] bg-blue-500 bottom-0 right-0 m-4 z-50 cursor-pointer rounded-full flex justify-center items-center theme-button"
         >
-        <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} className="w-[25px] h-[25px]" />
+        <FontAwesomeIcon icon={theme === "dark" ? faMoon : faSun} className="w-[25px] h-[25px]" />
         </div>
     );
 }
